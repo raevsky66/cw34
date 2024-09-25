24f60b5b82c3c760f33a855a487c9808e16ad65a
raevsky66 <raevsky66@gmail.com>
2024-09-26 01:12:55 +0300

    comm #1

--- src/telran/webinar/LineDataHandler.java
+++ src/telran/webinar/LineDataHandler.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
